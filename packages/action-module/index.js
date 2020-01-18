869a3aa899b5c4fd422cf90b0d5bbc24b694974a
Kaushal Regmi <regmikaushal321@gmail.com>
2020-01-19 01:12:09 +0545

    hospital-setup-actiondone complete

--- packages/action-module/index.js
+++ packages/action-module/index.js
@@ -1,13 +1,27 @@
-import {loginActionConstant, LoginActions,userMenusActionConstant,MenuActions} from './src/common';
-import {profileSetupConstants, ProfileSetupActions,SpecializationSetupActions,specializationSetupConstants} from './src/admin-module';
+import {
+  loginActionConstant,
+  LoginActions,
+  userMenusActionConstant,
+  MenuActions
+} from './src/common'
+import {
+  profileSetupConstants,
+  ProfileSetupActions,
+  SpecializationSetupActions,
+  specializationSetupConstants,
+  HospitalSetupActions,
+  hospitalSetupConstants
+} from './src/admin-module'
 
 export {
-    loginActionConstant,
-    LoginActions,
-    profileSetupConstants,
-    ProfileSetupActions,
-    userMenusActionConstant,
-    MenuActions,
-    specializationSetupConstants,
-    SpecializationSetupActions
+  loginActionConstant,
+  LoginActions,
+  profileSetupConstants,
+  ProfileSetupActions,
+  userMenusActionConstant,
+  MenuActions,
+  specializationSetupConstants,
+  SpecializationSetupActions,
+  hospitalSetupConstants,
+  HospitalSetupActions
 }
--- packages/action-module/src/admin-module/admin.js
+++ packages/action-module/src/admin-module/admin.js
@@ -2,10 +2,14 @@ import {profileSetupConstants} from "./profile-setup/profileSetupActionConstants
 import * as ProfileSetupActions from './profile-setup/profileSetupActions';
 import {specializationSetupConstants} from "./specialization-setup/specializationSetupConstant";
 import * as SpecializationSetupActions from './specialization-setup/specializationSetupConstant';
+import {hospitalSetupConstants} from "./hospital-setup/hospitalSetupConstant";
+import * as HospitalSetupActions from './hospital-setup/hospitalSetupActions';
 
 export {
     profileSetupConstants,
     ProfileSetupActions,
     specializationSetupConstants,
-    SpecializationSetupActions
+    SpecializationSetupActions,
+    hospitalSetupConstants,
+    HospitalSetupActions
 }
