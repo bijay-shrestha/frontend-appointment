b6ea520a83441b754c66e6b4b5793b961a28412b
Kaushal Regmi <regmikaushal321@gmail.com>
2020-02-11 00:45:42 +0545

    all appointment action done

--- packages/action-module/src/admin-module/appointment-details/appointmentDetailActions.js
+++ packages/action-module/src/admin-module/appointment-details/appointmentDetailActions.js
@@ -4,7 +4,19 @@ const {
     REFUND_FETCH_START,
     REFUND_FETCH_SUCCESS,
     REFUND_FETCH_ERROR,
-    CLEAR_REFUND_LIST_MESSAGE
+    CLEAR_REFUND_LIST_MESSAGE,
+    APPROVAL_FETCH_ERROR,
+    APPROVAL_FETCH_START,
+    APPROVAL_FETCH_SUCCESS,
+    CLEAR_APPROVAL_LIST_MESSAGE,
+    CLEAR_LOG_LIST_MESSAGE,
+    CLEAR_STATUS_LIST_MESSAGE,
+    LOG_FETCH_ERROR,
+    LOG_FETCH_START,
+    LOG_FETCH_SUCCESS,
+    STATUS_FETCH_ERROR,
+    STATUS_FETCH_START,
+    STATUS_FETCH_SUCCESS
 } = appointmentDetailsConstants;
 
 export const appointmentRefundFetchingStart = () => {
@@ -35,3 +47,86 @@ export const clearAppointmentSuccessMessage = () => {
     }
 };
 
+export const appointmentApprovalFetchingStart = () => {
+    return {
+        type: APPROVAL_FETCH_START
+    }
+};
+
+export const appointmentApprovalFetchingSuccess = data => {
+    return {
+        type: APPROVAL_FETCH_SUCCESS,
+        payload:data
+    }
+};
+
+export const appointmentApprovalFetchingError = message => {
+    return {
+        type: APPROVAL_FETCH_ERROR,
+        payload: {
+            data: message
+        }
+    }
+};
+
+export const clearAppointmentApprovalMessage = () => {
+    return {
+        type: CLEAR_APPROVAL_LIST_MESSAGE
+    }
+};
+
+export const appointmentLogFetchingStart = () => {
+    return {
+        type: LOG_FETCH_START
+    }
+};
+
+export const appointmentLogFetchingSuccess = data => {
+    return {
+        type: LOG_FETCH_SUCCESS,
+        payload:data
+    }
+};
+
+export const appointmentLogFetchingError = message => {
+    return {
+        type: LOG_FETCH_ERROR,
+        payload: {
+            data: message
+        }
+    }
+};
+
+export const clearAppointmentLogMessage = () => {
+    return {
+        type: CLEAR_LOG_LIST_MESSAGE
+    }
+};
+
+export const appointmentStatusFetchingStart = () => {
+    return {
+        type: STATUS_FETCH_START
+    }
+};
+
+export const appointmentStatusFetchingSuccess = data => {
+    return {
+        type: STATUS_FETCH_SUCCESS,
+        payload:data
+    }
+};
+
+export const appointmentStatusFetchingError = message => {
+    return {
+        type: STATUS_FETCH_ERROR,
+        payload: {
+            data: message
+        }
+    }
+};
+
+export const clearAppointmentStatusMessage = () => {
+    return {
+        type: CLEAR_APPROVAL_Status_MESSAGE
+    }
+};
