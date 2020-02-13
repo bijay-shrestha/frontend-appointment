ad52fcd1708a101e68368606e6b8ad7f8facf051
Cogent Health <apple@Apples-iMac-2.local>
2020-02-13 17:58:18 +0545

    appointment status

--- packages/helpers/src/appointment-status.js
+++ packages/helpers/src/appointment-status.js
@@ -1,7 +1,7 @@
 export const appointmentStatusList = [
-    {value: 'ALL', label: 'All'},
-    {value: 'V', label: 'Vacant'},
-    {value: 'PA', label: 'Pending Approval'},
-    {value: 'A', label: 'Approved'},
-    {value: 'C', label: 'Cancelled'}
+    {value: 'ALL', label: 'All',variant:''},
+    {value: 'V', label: 'Vacant',variant:'success'},
+    {value: 'PA', label: 'Pending Approval',variant:'warning'},
+    {value: 'A', label: 'Approved',variant:'danger'},
+    {value: 'C', label: 'Cancelled',variant:'dark'}
 ];
