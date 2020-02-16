9d567e8cce2d59e12a72a8c871545f467c63adfa
Kaushal Regmi <regmikaushal321@gmail.com>
2020-02-16 16:45:42 +0545

    done

--- packages/thunk-middleware/src/admin-middleware/patient-details-middleware/patientDetailsMiddleware.js
+++ packages/thunk-middleware/src/admin-middleware/patient-details-middleware/patientDetailsMiddleware.js
@@ -15,3 +15,18 @@ export const fetchPatientMetaList = (path,id) => async dispatch => {
 export const clearPatientDetails = () => async dispatch => {
     dispatch(PatientDetailsActions.clearPatientDropdownSuccessMessage());
 }
+
+export const searchPatientList = (path,data,pagination) => async dispatch => {
+    dispatch(PatientDetailsActions.patientSearchStart());
+    try {
+        const response = await Axios.putWithPagination(path,data,pagination);
+        dispatch(PatientDetailsActions.patientActiveDropdownFetchingSuccess(response.data));
+        return response.data;
+    } catch (e) {
+        dispatch(PatientDetailsActions.patientActiveDropdownFetchingError(e.errorMessage||"Sorry Internal Server Problem"));
+    }
+};
+
+export const clearPatientEdit = () => async dispatch => {
+    dispatch(PatientDetailsActions.clearPatientEditMessage());
+}
