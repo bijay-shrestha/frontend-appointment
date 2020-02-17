69e0c4f086440fcca53fc910d2bfe42fd11f5ea9
Kaushal Regmi <regmikaushal321@gmail.com>
2020-02-17 10:34:45 +0545

    done

--- packages/thunk-middleware/src/admin-middleware/patient-details-middleware/patientDetailsMiddleware.js
+++ packages/thunk-middleware/src/admin-middleware/patient-details-middleware/patientDetailsMiddleware.js
@@ -1,43 +1,76 @@
-import {PatientDetailsActions} from "@frontend-appointment/action-module";
-import {Axios} from "@frontend-appointment/core";
+import {PatientDetailsActions} from '@frontend-appointment/action-module'
+import {Axios} from '@frontend-appointment/core'
 
-export const fetchPatientMetaList = (path,id) => async dispatch => {
-    dispatch(PatientDetailsActions.patientActiveDropdownFetchingStart());
-    try {
-        const response = await Axios.getWithPathVariables(path,id);
-        dispatch(PatientDetailsActions.patientActiveDropdownFetchingSuccess(response.data));
-        return response.data;
-    } catch (e) {
-        dispatch(PatientDetailsActions.patientActiveDropdownFetchingError(e.errorMessage||"Sorry Internal Server Problem"));
-    }
-};
+export const fetchPatientMetaDropdown = (path, id) => async dispatch => {
+  dispatch(PatientDetailsActions.patientActiveDropdownFetchingStart())
+  try {
+    const response = await Axios.getWithPathVariables(path, id)
+    dispatch(
+      PatientDetailsActions.patientActiveDropdownFetchingSuccess(response.data)
+    )
+    return response.data
+  } catch (e) {
+    dispatch(
+      PatientDetailsActions.patientActiveDropdownFetchingError(
+        e.errorMessage || 'Sorry Internal Server Problem'
+      )
+    )
+  }
+}
 
 export const clearPatientDetails = () => async dispatch => {
-    dispatch(PatientDetailsActions.clearPatientDropdownSuccessMessage());
+  dispatch(PatientDetailsActions.clearPatientDropdownSuccessMessage())
 }
 
-export const editPatient = (path,data) => async dispatch => {
-    dispatch(PatientDetailsActions.patientEditPending());
-    try {
-        const response = await Axios.put(path,data);
-        dispatch(PatientDetailsActions.patientEditSuccess(response.data));
-        return response.data;
-    } catch (e) {
-        dispatch(PatientDetailsActions.patientEditError(e.errorMessage||"Sorry Internal Server Problem"));
-    }
-};
+export const editPatient = (path, data) => async dispatch => {
+  dispatch(PatientDetailsActions.patientEditPending())
+  try {
+    const response = await Axios.put(path, data)
+    dispatch(PatientDetailsActions.patientEditSuccess(response.data))
+    return response.data
+  } catch (e) {
+    dispatch(
+      PatientDetailsActions.patientEditError(
+        e.errorMessage || 'Sorry Internal Server Problem'
+      )
+    )
+  }
+}
 
 export const clearPatientPreview = () => async dispatch => {
-    dispatch(PatientDetailsActions.clearPatientPreviewMessage());
+  dispatch(PatientDetailsActions.clearPatientPreviewMessage())
 }
 
-export const previewPatient = (path,data) => async dispatch => {
-    dispatch(PatientDetailsActions.patientPreviewPending());
-    try {
-        const response = await Axios.put(path,data);
-        dispatch(PatientDetailsActions.patientPreviewSuccess(response.data));
-        return response.data;
-    } catch (e) {
-        dispatch(PatientDetailsActions.patientPreviewError(e.errorMessage||"Sorry Internal Server Problem"));
-    }
-};
+export const previewPatient = (path, data) => async dispatch => {
+  dispatch(PatientDetailsActions.patientPreviewPending())
+  try {
+    const response = await Axios.put(path, data)
+    dispatch(PatientDetailsActions.patientPreviewSuccess(response.data))
+    return response.data
+  } catch (e) {
+    dispatch(
+      PatientDetailsActions.patientPreviewError(
+        e.errorMessage || 'Sorry Internal Server Problem'
+      )
+    )
+  }
+}
+
+export const fetchPatientMetaList = (
+  path,
+  searchData,
+  pagination
+) => async dispatch => {
+  dispatch(PatientDetailsActions.patientSearchStart())
+  try {
+    const response = await Axios.putWithPagination(path, pagination, searchData)
+    dispatch(PatientDetailsActions.patientSearchSuccess(response.data))
+    return response.data
+  } catch (e) {
+    dispatch(
+      PatientDetailsActions.patientSearchError(
+        e.errorMessage || 'Sorry Internal Server Problem'
+      )
+    )
+  }
+}
