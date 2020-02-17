b47368c92c46495497fd0a7e6ba0907cc71bbcf9
Kaushal Regmi <regmikaushal321@gmail.com>
2020-02-17 22:12:50 +0545

    Login Hoc Done

--- packages/commons/src/LoginHOC.js
+++ packages/commons/src/LoginHOC.js
@@ -1,29 +1,34 @@
 import React from 'react'
-import {Axios} from '@frontend-appointment/core'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom';
-import {CLoading} from '@frontend-appointment/ui-components';
-import {CFullPageLoading} from '@frontend-appointment/ui-elements';
-import {LocalStorageSecurity} from '@frontend-appointment/helpers';
+import {Redirect} from 'react-router-dom'
+import {
+  LocalStorageSecurity,
+  EnvironmentVariableGetter
+} from '@frontend-appointment/helpers'
 
 const LoginHoc = (ComposedComponent, dashboardPath) => {
-    const  CheckAuthentication = props =>{
-     let path=dashboardPath;
-     let isLoggedIn = false;
-     const checkIfUserIsLoggedInAlready =()=> {
-         if(LocalStorageSecurity.localStorageDecoder('auth-token')){
-             isLoggedIn=true;
-             path = ''
-         }
-     }   
-
-            return !this.state.isLoading && !this.state.isOk ? (
-                <ComposedComponent {...this.props} history={this.props.history}/>
-            ) : this.state.isLoading && !this.state.isOk ? (
-                <CFullPageLoading/>
-            ) : (
-                <Redirect to={dashboardPath}/>
-            )
-        }
+  const CheckAuthentication = props => {
+    let path = dashboardPath
+    let isLoggedIn = false
+    const checkIfUserIsLoggedInAlready = () => {
+      if (LocalStorageSecurity.localStorageDecoder('auth-token')) {
+        isLoggedIn = true
+        const pathInLocalStorage = LocalStorageSecurity.localStorageDecoder(
+          'active'
+        )
+        if (pathInLocalStorage)
+          path =
+            EnvironmentVariableGetter.REACT_APP_BASE_PATH_CODE +
+            pathInLocalStorage.replace('true', '')
+      }
+      return isLoggedIn
     }
-export default LoginHoc;
+
+    return !checkIfUserIsLoggedInAlready() ? (
+      <ComposedComponent {...props} history={props.history} />
+    ) : (
+      <Redirect to={path} />
+    )
+  }
+  return CheckAuthentication
+}
+export default LoginHoc
