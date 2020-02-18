5a1ef0f2c8487b4bedf3594c5d37d7225ad95a5d
sabu shakya <sabu.shakya@f1soft.com>
2020-02-18 11:41:55 +0545

    Change environment variables
    Took 24 minutes

--- packages/helpers/src/utils/EnvironmentVariableGetter.js
+++ packages/helpers/src/utils/EnvironmentVariableGetter.js
@@ -1,6 +1,19 @@
-export const SERVER_DOMAIN = process.env.REACT_APP_SERVER_DOMAIN || ''
-export const APP_PORT = process.env.PORT || ''
-export const REACT_APP_BASE_PATH_CODE =process.env.REACT_APP_BASE_PATH_CODE||''
-export const REACT_APP_MODULE_CODE=process.env.REACT_APP_MODULE_CODE||''
-export const REACT_APP_IMAGE_UPLOAD_SIZE_LIMIT=process.env.REACT_APP_IMAGE_UPLOAD_SIZE_LIMIT||''
-export const REACT_APP_HOSPITAL_CODE=process.env.REACT_APP_HOSPITAL_CODE||''
+export const SERVER_DOMAIN = process.env.REACT_APP_SERVER_DOMAIN || '';
+export const REACT_APP_BASE_PATH_CODE = process.env.REACT_APP_BASE_PATH_CODE || '';
+export const REACT_APP_MODULE_CODE = process.env.REACT_APP_MODULE_CODE || '';
+
+export const APP_PORT = process.env.PORT || '';
+
+export const REACT_APP_IMAGE_UPLOAD_SIZE_LIMIT = process.env.REACT_APP_IMAGE_UPLOAD_SIZE_LIMIT || '';
+
+export const ADMIN_SERVER_DOMAIN = process.env.REACT_APP_ADMIN_SERVER_DOMAIN || '';
+export const CLIENT_SERVER_DOMAIN = process.env.REACT_APP_CLIENT_SERVER_DOMAIN || '';
+
+export const ADMIN_PORT = process.env.REACT_APP_ADMIN_PORT || '';
+export const CLIENT_PORT =  process.env.REACT_APP_CLIENT_PORT || '';
+
+export const ADMIN_MODULE_CODE = process.env.REACT_APP_MODULE_CODE || '';
+export const CLIENT_MODULE_CODE = process.env.REACT_APP_CLIENT_MODULE_CODE || '';
+
+export const ADMIN_AUTH_TOKEN = process.env.REACT_APP_ADMIN_AUTH_TOKEN || '';
+export const CLIENT_AUTH_TOKEN =  process.env.REACT_APP_CLIENT_AUTH_TOKEN || '';
