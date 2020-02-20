a8af9b2c8b1f0bef9f1cc8f730be7bd440373253
sabu shakya <sabu.shakya@f1soft.com>
2020-02-20 09:41:14 +0545

    Add appointment queue api
    Took 29 minutes

--- packages/web-resource-key-constants/src/adminModuleWebResourceKeyConstants.js
+++ packages/web-resource-key-constants/src/adminModuleWebResourceKeyConstants.js
@@ -151,7 +151,7 @@ export const appointmentSetupApiConstant ={
     APPOINTMENT_STATUS_LIST:BASE.concat(APPOINTMENT_BASE+"/status"),
     APPOINTMENT_APPROVAL_LIST:BASE.concat(APPOINTMENT_BASE+"/pending-approval"),
     APPOINTMENT_APPROVE:BASE.concat(APPOINTMENT_BASE+"/approve"),
-    APPOINTMENT_REJECT:BASE.concat(APPOINTMENT_BASE+"/reject"), 
+    APPOINTMENT_REJECT:BASE.concat(APPOINTMENT_BASE+"/reject"),
     SEARCH_APPOINTMENT_RESCHEDULE: BASE.concat(APPOINTMENT_BASE.concat("/reschedule-log"))
 };
 
@@ -159,7 +159,8 @@ export const DashboardApiConstant = {
     OVERALL_APPOINTMENTS: BASE.concat(DASHBOARD_BASE + "/overAllAppointments"),
     REGISTERED_PATIENTS: BASE.concat(DASHBOARD_BASE + "/registeredPatients/count"),
     REVENUE_GENERATED: BASE.concat(DASHBOARD_BASE + "/revenueGenerated"),
-    REVENUE_STATISTICS: BASE.concat(DASHBOARD_BASE + "/revenueStatistics")
+    REVENUE_STATISTICS: BASE.concat(DASHBOARD_BASE + "/revenueStatistics"),
+    APPOINTMENT_QUERY:BASE.concat(DASHBOARD_BASE + "/today-appointment")
 };
 
 
