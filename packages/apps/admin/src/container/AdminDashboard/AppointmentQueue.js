5e8517616cd1451cdac2f4497c70359a59727df9
Cogent Health <apple@Apples-iMac-2.local>
2020-02-27 16:04:18 +0545

    app-queue

--- packages/apps/admin/src/container/AdminDashboard/AppointmentQueue.js
+++ packages/apps/admin/src/container/AdminDashboard/AppointmentQueue.js
@@ -23,7 +23,7 @@ const AppointmentQueue = props => {
     <div className="app-log">
       <Row>
         <Col className="px-0">
-          <Form className="hospital-list">
+          <Form className="hospital-list float-left">
             <Form.Group as={Row} controlId="formPlaintextEmail">
               <Col sm="12">
                 <div className="hospital-list-input">
