1b75f8d4915ea37d33f6353edbd2349039983eaf
Kaushal Regmi <regmikaushal321@gmail.com>
2020-03-08 15:28:46 +0545

    Api added for password and code verification

--- packages/web-resource-key-constants/src/commonWebResourceKeyConstants.js
+++ packages/web-resource-key-constants/src/commonWebResourceKeyConstants.js
@@ -6,6 +6,8 @@ export const WeekdaysApiConstants = {
     FETCH_WEEKDAYS_DATA: BASE.concat(WEEK_DAYS_BASE.concat('/prepare-weekdays-data')),
 };
 
+const PASSWORD_BASE='/password'
 export const ForgotPasswordAndVerification = {
-    
+    FORGOT_PASSWORD:BASE.concat(PASSWORD_BASE.concat('/forgot')),
+    CODE_VERIFICATION:BASE.concat(PASSWORD_BASE.concat('/verify'))
 }
