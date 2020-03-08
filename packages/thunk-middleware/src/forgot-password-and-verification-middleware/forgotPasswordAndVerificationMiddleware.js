7b6827789cff71c1ae1583f4c5f9144373ff1436
Kaushal Regmi <regmikaushal321@gmail.com>
2020-03-08 15:22:25 +0545

    middleware-done-for-forgot-and-verification

---
+++ packages/thunk-middleware/src/forgot-password-and-verification-middleware/forgotPasswordAndVerificationMiddleware.js
@@ -0,0 +1,30 @@
+import {Axios} from '@frontend-appointment/core'
+import {ForgotPasswordActions} from '@frontend-appointment/action-module'
+
+export const ForgotPassword = (path, data) => async dispatch => {
+  dispatch(forgotPasswordActions.isForgotPasswordPending())
+  try {
+    await Axios.postWithRequestParams(path, data)
+    dispatch(
+      ForgotPasswordActions.isForgotPasswordSuccess(
+        'Password Forgot Successfully'
+      )
+    )
+  } catch (e) {
+    dispatch(ForgotPasswordActions.isForgotPasswordError(e.errorMessage))
+  }
+}
+
+export const codeVerification = (path, data) => async dispatch => {
+  dispatch(ForgotPasswordActions.isForgotPasswordPending())
+  try {
+    await Axios.getWithRequestParams(path, data)
+    dispatch(
+      ForgotPasswordActions.isVerificationCodeSuccess(
+        'Password Verified Successfully'
+      )
+    )
+  } catch (e) {
+    dispatch(ForgotPasswordActions.isVerificationCodeSuccess(e.errorMessage))
+  }
+}
