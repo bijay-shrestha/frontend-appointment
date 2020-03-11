bbd24503991106abbebf87e4398e01ba00071b93
Kaushal Regmi <regmikaushal321@gmail.com>
2020-03-11 10:21:58 +0545

    done

--- packages/core/src/axios/axios-helper/api-error.js
+++ packages/core/src/axios/axios-helper/api-error.js
@@ -1,26 +1,27 @@
 export default {
-  errorHandler: error => {
-    const {response} = error
-    const status = response ? response.status : ''
-    let errorObj = {}
-    switch (status) {
-      case 400:
-      case 401:
-      case 403:
-      case 404:
-      case 409:
-      case 417:
-      case 500:
-      case 502:
-        console.log('Developer Api Error:', error)
-        errorObj = {...error.response.data}
-      default:
-        console.log('Developer Api Error:', error)
-        errorObj = {
-          errorMessage: error.message || error.errorMessage,
-          stack: error.stack
+    errorHandler: error => {
+        const {response} = error;
+        const status = response ? response.status : '';
+
+        switch (status) {
+            case 400:
+            case 401:
+            case 403:
+            case 404:
+            case 409:
+            case 417:
+            case 500:
+            case 502:
+                console.log("Developer Api Error:", error);
+                throw error.response.data;
+            default:
+                console.log("Developer Api Error:", error);
+                let errorObj = {
+                    errorMessage: error.message||error.errorMessage,
+                    stack: error.stack
+                };
+                throw errorObj;
         }
     }
-    throw errorObj
-  }
+
 }
