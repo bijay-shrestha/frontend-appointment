bf0a9beb85afc325cd3e3db3ecd1b852843cab2e
Kaushal Regmi <regmikaushal321@gmail.com>
2020-03-16 14:59:13 +0545

    done

---
+++ packages/apps/admin/src/container/AdminDashboard/DoctorRevenueList.jsx
@@ -0,0 +1,174 @@
+import React, {memo} from 'react'
+import {Row, Col, Form} from 'react-bootstrap'
+import {
+  CDataTable,
+  CHybridSelect,
+  CLoading,
+  CPagination
+} from '@frontend-appointment/ui-elements'
+import DoctorWithSpecializationAndImage from '../CommonComponents/table-components/DoctorWithSpecializationAndImage'
+import PatientWithMobileNumber from '../CommonComponents/table-components/PatientNameWithMobileNumber'
+const DoctorRevenueList = props => {
+  const {
+    isDoctorRevenueLoading,
+    doctorRevenueData,
+    doctorRevenueErrorMessage,
+    totalRecords,
+    queryParams,
+    handlePageChange,
+    doctorId,
+    handleDoctorChange,
+    doctorDropdown,
+    fromDate,
+    toDate
+  } = props.doctorRevenue
+  return (
+    <>
+      {props.hospitalId.value ? (
+        <Row className="mt-4">
+          <h5 className="title">Doctor Revenue</h5>
+          <div className="app-log">
+            <Row>
+              <Col className="px-0">
+                <Form className="hospital-list float-left">
+                  <Form.Group as={Row} controlId="formPlaintextEmail">
+                    <Col sm="12">
+                      <div className="hospital-list-input">
+                        <CHybridSelect
+                          name="doctorId"
+                          placeholder="Select Doctor"
+                          onChange={handleDoctorChange}
+                          options={doctorDropdown}
+                          value={doctorId}
+                        ></CHybridSelect>
+                      </div>
+                      <div className="d-flex">
+                        <CEnglishDatePicker
+                          id="from-date"
+                          name="fromDate"
+                          label="From Date"
+                          dateFormat="yyyy-MM-dd"
+                          // minDate={0}
+                          showDisabledMonthNavigation={true}
+                          selected={fromDate}
+                          peekNextMonth={true}
+                          showMonthDropdown={true}
+                          showYearDropdown={true}
+                          dropdownMode="select"
+                          onKeyDown={event => this.handleEnter(event)}
+                          onChange={date =>
+                            onSearchInputChange(date, 'fromDate')
+                          }
+                        />{' '}
+                        &nbsp;&nbsp;
+                        <CEnglishDatePicker
+                          id="to-date"
+                          name="toDate"
+                          label="To Date"
+                          dateFormat="yyyy-MM-dd"
+                          showDisabledMonthNavigation={true}
+                          selected={toDate}
+                          peekNextMonth={true}
+                          showMonthDropdown={true}
+                          showYearDropdown={true}
+                          dropdownMode="select"
+                          onChange={date => onSearchInputChange(date, 'toDate')}
+                        />
+                      </div>
+                    </Col>
+                  </Form.Group>
+                </Form>
+              </Col>
+
+              <Col className="date">
+                <div>
+                  <span>Date :</span> {new Date().toDateString()}
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <div className="app-queue-datatable">
+                {!isDoctorRevenueLoading &&
+                !doctorRevenueErrorMessage &&
+                doctorRevenueData.length ? (
+                  <>
+                    <CDataTable
+                      classes="ag-theme-balham"
+                      id="roles-table"
+                      width="100%"
+                      height="360px"
+                      enableSorting
+                      editType
+                      rowHeight={50}
+                      columnDefs={[
+                        {
+                          headerName: 'Doctor(Specialization)',
+                          resizable: true,
+                          sortable: true,
+                          sizeColumnsToFit: true,
+                          cellRenderer: 'doctorwithSpecializationRenderer'
+                        },
+
+                        {
+                          headerName: 'Patient(Mobile Number)',
+                          resizable: true,
+                          sortable: true,
+                          sizeColumnsToFit: true,
+                          cellRenderer: 'mobileRender'
+                        },
+                        {
+                          headerName: 'Appointment Time',
+                          field: 'appointmentTime',
+                          resizable: true,
+                          sortable: true,
+                          sizeColumnsToFit: true
+                        }
+                      ]}
+                      frameworkComponents={{
+                        doctorwithSpecializationRenderer: DoctorWithSpecializationAndImage,
+                        mobileRender: PatientWithMobileNumber
+                      }}
+                      defaultColDef={{resizable: true}}
+                      rowSelection={'single'}
+                      rowData={appointmentQueueData}
+                      // rowModelType= "infinite"
+                      // paginationPageSize={6}
+                      // cacheOverflowSize={2}
+                      // maxConcurrentDatasourceRequests={1}
+                      // infiniteInitialRowCount={6}
+                      // maxBlocksInCache={6}
+                      // dataSource={()=>handlePageChange(queryParams.page++)}
+                    />
+
+                    <CPagination
+                      totalItems={totalRecords}
+                      maxSize={queryParams.size}
+                      currentPage={queryParams.page}
+                      onPageChanged={handlePageChange}
+                    />
+                  </>
+                ) : !isAppointmentQueueLoading &&
+                  appointmentQueueErrorMessage ? (
+                  <div className="filter-message">
+                    <div className="no-data">
+                      <i className="fa fa-file-text-o"></i>
+                    </div>
+                    <div className="message">
+                      {' '}
+                      {appointmentQueueErrorMessage}
+                    </div>
+                  </div>
+                ) : (
+                  <CLoading />
+                )}
+              </div>
+            </Row>
+          </div>
+        </Row>
+      ) : (
+        ''
+      )}
+    </>
+  )
+}
+export default memo(DoctorRevenueList)
