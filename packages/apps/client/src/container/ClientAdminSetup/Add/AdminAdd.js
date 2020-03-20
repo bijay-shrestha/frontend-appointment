202424ff229cf2ff1d2543c118305b131f26d24c
sabu shakya <sabu.shakya@f1soft.com>
2020-03-20 10:16:19 +0545

    Add production configs and change baseUrl to single serverDomain in admin create
    Took 29 minutes

--- packages/apps/client/src/container/ClientAdminSetup/Add/AdminAdd.js
+++ packages/apps/client/src/container/ClientAdminSetup/Add/AdminAdd.js
@@ -259,7 +259,8 @@ class AdminAdd extends PureComponent {
             macAddressInfo: macIdList.length ? macIdList.map(macId => {
                 return macId.macId
             }) : [],
-            baseUrl: EnvironmentVariableGetter.CLIENT_SERVER_DOMAIN.concat(":".concat(EnvironmentVariableGetter.CLIENT_PORT))
+            baseUrl: EnvironmentVariableGetter.CLIENT_SERVER_DOMAIN
+            // baseUrl: EnvironmentVariableGetter.CLIENT_SERVER_DOMAIN.concat(":".concat(EnvironmentVariableGetter.CLIENT_PORT))
         };
 
         let formData = new FormData();
