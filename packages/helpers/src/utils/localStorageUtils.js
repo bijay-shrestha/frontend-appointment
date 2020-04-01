8d322e95f4168d86d9d2bb7dc8538fca2636858b
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-01 13:05:32 +0545

    key done

--- packages/helpers/src/utils/localStorageUtils.js
+++ packages/helpers/src/utils/localStorageUtils.js
@@ -1,35 +1,38 @@
-import SecureLS from 'secure-ls'
+import SecureLS from 'secure-ls';
+import {APP_SECRET_KEY} from './EnvironmentVariableGetter'
 const env =process.env.NODE_ENV;
-const encodingType=env.toString() === "development"?'':'aes';
-const isCompression=env.toString() === "development"?false:true;
-const ls = new SecureLS({encodingType:encodingType ,isCompression:isCompression})
+const encodingType='des'//env.toString() === "development"?'':'aes';
+const isCompression=true//env.toString() === "development"?false:true;
+const encryptionSecret=APP_SECRET_KEY;
+const ls = new SecureLS({encodingType: encodingType, isCompression: isCompression, encryptionSecret: encryptionSecret})
 export const localStorageSecurity = {
-  localStorageDecoder: key => {
+  localStorageDecoder:  key => {
     try {
-      return ls.get(key)
+      const newKey  = ls.get(key);
+      return newKey;
     } catch (e) {
       return false
     }
   },
   localStorageEncoder: (key, encodeValue) => {
     try {
-      ls.set(key, encodeValue)
+     ls.set(key, encodeValue)
     } catch (e) {
       console.log('localstorage setting error', e)
     }
   },
   localStorageRemover: () => {
     try {
-     ls.removeAll()
-    ls.remove('_secure__ls__metadata')
+      ls.removeAll()
+      ls.remove('_secure__ls__metadata')
     } catch (e) {
       console.log('localstorage removing error', e)
     }
   },
   particularValueRemover: key => {
     try {
-      ls.remove(key)
-      ls.remove('_secure__ls__metadata')
+       ls.remove(key)
+       ls.remove('_secure__ls__metadata')
     } catch (e) {
       console.log('localstorage removing error', e)
     }
