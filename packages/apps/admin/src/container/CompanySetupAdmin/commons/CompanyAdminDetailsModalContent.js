b63d265a7a9c7be2188f2518cf06c2331097b23b
sudinshakya <sudinshakya1@gmail.com>
2020-04-06 14:34:58 +0545

    company-admin-setup

--- packages/apps/admin/src/container/CompanySetupAdmin/commons/CompanyAdminDetailsModalContent.js
+++ packages/apps/admin/src/container/CompanySetupAdmin/commons/CompanyAdminDetailsModalContent.js
@@ -170,7 +170,7 @@ const AdminDetailsModalContent = ({adminInfoObj, adminImage}) => {
                     )}
                   </Col>
 
-                  <Col sm={12} md={12} lg={6} className="py-4 dash-roles-container">
+                  <Col sm={12} md={12} lg={6} className="dash-roles-container">
                     {adminInfoObj.adminDashboardRequestDTOS && adminInfoObj.adminDashboardRequestDTOS.length ? (
                       <CFLabel labelName="Dashboard Role" id="dashboard-role" />
                     ) : null}
