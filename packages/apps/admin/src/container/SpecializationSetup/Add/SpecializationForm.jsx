093798d237e1f0e40905f56e1c2bcaf5cddc481e
sabu shakya <sabu.shakya@f1soft.com>
2020-04-07 11:15:51 +0545

    Change hospital to client in Specialization
    Took 8 minutes

--- packages/apps/admin/src/container/SpecializationSetup/Add/SpecializationForm.jsx
+++ packages/apps/admin/src/container/SpecializationSetup/Add/SpecializationForm.jsx
@@ -21,14 +21,14 @@ const SpecializationForm = ({
                         <Row>
                             <Col sm={12} md={6} lg={4}>
                                 <CHybridSelect
-                                    id="hospital"
-                                    label="Hospital"
+                                    id="client"
+                                    label="Client"
                                     name="hospitalId"
                                     onKeyDown={onEnterKeyPress}
                                     onChange={(event, validity) => onInputChange(event, validity)}
                                     options={hospitalList}
                                     value={specializationInfoObj.hospitalId}
-                                    placeholder={'Select Hospital.'}
+                                    placeholder={'Select Client.'}
                                 />
                             </Col>
 
