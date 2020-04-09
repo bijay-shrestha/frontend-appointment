8b49391037110ffe3f84267d2c6ed77073c81934
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-09 12:52:11 +0545

    done

--- packages/ui-components/src/CLayout/components/CHeader.js
+++ packages/ui-components/src/CLayout/components/CHeader.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import {Badge, Button, Dropdown, Form, Image} from 'react-bootstrap';
+import {Badge, Button, Dropdown, Image} from 'react-bootstrap';
 import {Axios} from '@frontend-appointment/core';
 import {CBreadcrumb, CDoubleShiftSearch} from '@frontend-appointment/ui-elements';
-
 import {AdminModuleAPIConstants} from '@frontend-appointment/web-resource-key-constants';
 import CChangePasswordModal from '../../CChangePassword/CChangePasswordModal';
-import {Link} from "react-router-dom";
 import {LocalStorageSecurity} from "@frontend-appointment/helpers";
 
 
