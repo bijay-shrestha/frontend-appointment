859f7eb4a9906640575e965a42f67822c4843d22
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-14 12:04:50 +0545

    done

---
+++ packages/apps/admin/src/AuthenticateHOC/StartupApiHoc.js
@@ -0,0 +1,16 @@
+import React,{PureComponent} from 'react';
+
+class StartupApiHoc extends PureComponent {
+   componentDidMount(){
+
+   }
+    render(){
+        const {component}=this.props
+       return (
+         <>
+         <h1>done</h1>
+         </>
+       )
+   }
+}
+export default StartupApiHoc;
