9ab9ee2586292fce0b9d97e2768186098b5ab19b
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-15 15:12:35 +0545

    Action type save in seesionStorage

--- packages/apps/admin/src/container/CommonComponents/table-components/TableApproveAction.js
+++ packages/apps/admin/src/container/CommonComponents/table-components/TableApproveAction.js
@@ -1,15 +1,19 @@
 import React, {PureComponent} from 'react'
 import {Dropdown} from 'react-bootstrap'
-import {CButton} from "@frontend-appointment/ui-elements";
+import {CButton} from '@frontend-appointment/ui-elements'
 import * as Material from 'react-icons/md'
 import * as Feather from 'react-icons/fi'
 import {ActionFilterUtils} from '@frontend-appointment/helpers'
 
-const {checkIfRoleExists} = ActionFilterUtils;
+const {checkIfRoleExists} = ActionFilterUtils
 const ApproveTableAction = props => {
+  const saveActionInSession = (e, actionId, actionName) => {
+    sessionStorage.setItem('actionType', actionId)
+    props.onClick(e, props.node.data.id || props.node.data, actionName)
+  }
   return (
     <>
-     {/* <CButton
+      {/* <CButton
      name=""
      variant="success"
               onClickHandler={e => props.onClick(e, props.node.data.id||props.node.data, 'E')}
@@ -24,10 +28,8 @@ const ApproveTableAction = props => {
         <Dropdown.Menu>
           {
             // checkIfRoleExists(this.props.filteredAction, 3) &&
-            <Dropdown.Item
-              onClick={e => props.onClick(e, props.node.data.id||props.node.data, 'E')}
-            >
-                   <i className="fa fa-sign-in"></i>&nbsp; Check-In{' '}
+            <Dropdown.Item onClick={e => saveActionInSession(e, 14, 'E')}>
+              <i className="fa fa-sign-in"></i>&nbsp; Check-In{' '}
             </Dropdown.Item>
           }
           {/* {
@@ -44,4 +46,4 @@ const ApproveTableAction = props => {
   )
 }
 
-export default ApproveTableAction;
+export default ApproveTableAction
