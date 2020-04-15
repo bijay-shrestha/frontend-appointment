06ed93425e6411ca8b5214c1f09fa4ad4b276544
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-15 15:21:22 +0545

    done

--- packages/apps/admin/src/container/CommonComponents/table-components/hoc/PreviewHandlerHoc.js
+++ packages/apps/admin/src/container/CommonComponents/table-components/hoc/PreviewHandlerHoc.js
@@ -13,12 +13,14 @@ const PreviewHandlerHoc = (
     const previewClickHandler = (e)=>{
         const data = node.data ? (node.data.id ? node.data.id : node.data) : null
         if (!checkIfRoleExists) {
+          
           onPreviewHandler(data)
         } else {
           if (checkIfRoleExists(filteredActions, roleId)) {
             onPreviewHandler(data)
           }
         }
+        sessionStorage.setItem('actionType',4);
     }
     return (ComposedComponent ? (
       <div
