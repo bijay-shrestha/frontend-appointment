3267dd9457a9af462e8d38c032b1cc7bf4dab022
sudinshakya <sudinshakya1@gmail.com>
2020-04-20 18:01:20 +0545

    client-dashbaord-applog

--- packages/apps/client/src/container/ClientAppointmentLog/AppointmentLog.js
+++ packages/apps/client/src/container/ClientAppointmentLog/AppointmentLog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AppointmentLogSearchFilter from './AppointmentLogSearchFilter'
 import AppointmentLogDataTable from './AppointmentLogDataTable'
 import AppointmentLogHoc from './AppointmentLogHoc'
+import { Table,Container,Col} from 'react-bootstrap';
 
 const AppointmentRefundLog = props => {
     const AppoinmentRefund = AppointmentLogHoc(
@@ -17,6 +18,99 @@ const AppointmentRefundLog = props => {
                         paginationProps={paginationProps}
                     />
                 </div>
+
+                <Container className="revenue-details" fluid>
+         
+         <div className="row">
+           <h5 className="rd-title">Revenue Details</h5>
+           </div>
+           <div className="row rd-container">
+          
+ 
+               
+             <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card book">
+               <div className="icon">
+                 B
+               </div>
+               <div className="rd-content ">
+               <span>   <span className="label">Booked</span> </span>
+               <span> <span className="amt"> NPR 50000</span> from<span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+ 
+               <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card checkin">
+               <div className="icon">
+                 CH
+               </div>
+               <div className="rd-content ">
+               <span>   <span className="label">Checked-In</span> </span>
+               <span> <span className="amt"> NPR 50000</span> from<span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+ 
+               <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card cancel">
+               <div className="icon">
+                 C
+               </div>
+               <div className="rd-content ">
+               <span>   <span className="label">Cancel</span> </span>
+               <span> <span className="amt"> NPR 50000</span> from <span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+ 
+               <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card refund">
+               <div className="icon">
+                 R
+               </div>
+               <div className="rd-content ">
+               <span>   <span className="label">Refund</span></span> 
+               <span> <span className="amt"> NPR 50000</span> from
+               <span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+ 
+               <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card refund-client">
+               <div className="icon">
+                 RF
+               </div>
+               <div className="rd-content">
+               <span>   <span className="label">Refunded Amount to Client </span> </span>
+               <span> <span className="amt"> NPR 50000</span> from
+               <span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+ 
+               <Col  md={4} className="p-0  mt-4">
+               <div className="rd-card total">
+               <div className="icon">
+                 T
+               </div>
+               <div className="rd-content ">
+               <span>   <span className="label">Total Revenue Amount from Client <br></br><span className="inc">(Incl. Booked Appts. revenue)</span> </span> </span>
+               <span> <span className="amt"> NPR 50000</span> from
+                <span className="apt"> 200 </span>Appointments</span> 
+               </div>
+               </div>
+             
+               </Col>
+           </div>
+         </Container>
+        
             </>
         ),
         props,
