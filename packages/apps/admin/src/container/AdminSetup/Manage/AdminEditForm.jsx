9f862da067a0d47da3eaeb8b41d4b3da7e7381f7
sabu shakya <sabu.shakya@f1soft.com>
2020-04-21 18:00:56 +0545

    Change Dashboard role to roles

--- packages/apps/admin/src/container/AdminSetup/Manage/AdminEditForm.jsx
+++ packages/apps/admin/src/container/AdminSetup/Manage/AdminEditForm.jsx
@@ -282,7 +282,7 @@ const AdminEditForm = ({
 
               <Col sm={12} md={12} lg={6} className="py-4 dash-roles-container">
                 {adminInfoObj.adminDashboardRequestDTOS.length ? (
-                  <CFLabel labelName="Dashboard Role" id="dash-role-edit" />
+                  <CFLabel labelName="Dashboard Roles" id="dash-role-edit" />
                 ) : null}
                 {adminInfoObj && adminInfoObj.adminDashboardRequestDTOS.length
                   ? adminInfoObj.adminDashboardRequestDTOS.map(
