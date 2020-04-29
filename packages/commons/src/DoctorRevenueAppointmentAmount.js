ea8da2b0b7548d5cb197017c7a3267c30e33e7ee
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-29 16:04:49 +0545

    done

--- packages/commons/src/DoctorRevenueAppointmentAmount.js
+++ packages/commons/src/DoctorRevenueAppointmentAmount.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export const DoctorRevenueAppointmentAmount = props => {
-  const {cancelledRevenue, doctorRevenue, totalRevenueAmount} = props.node.data
+  const {cancelledRevenue, doctorRevenue, totalRevenue} = props.node.data
   return (
     <div>
-      <p id="revenueAmountId" className="showTotal">Total Revenue Amount:totalRevenueAmount}</p>
+      <p id="revenueAmountId" className="showTotal">Total Revenue Amount:totalRevenue}</p>
       <ul id="breakDownAmountId" className="hideTotal">
         <li>Successfull Revenue:{cancelledRevenue}</li>
         <li>Cancelled Revenue:{doctorRevenue}</li>
