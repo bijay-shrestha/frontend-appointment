2601cb4aa30ffe9100f60c275bdff19ace897367
Kaushal Regmi <regmikaushal321@gmail.com>
2020-04-29 13:23:19 +0545

    done

--- packages/commons/src/DoctorRevenueNoOfAppointments.js
+++ packages/commons/src/DoctorRevenueNoOfAppointments.js
@@ -1,9 +1,12 @@
-export const DoctorRevenueAppointmentAmount = props => {
-    const {successfulAppointments,cancelledAppointments}=props.node.data 
+export const DoctorRevenueNoOfAppointments = props => {
+    const {successfulAppointments,cancelledAppointments,totalAppointments}=props.node.data 
      return(
-       <ul>
+       <div>
+       <p id="appointmentNumberId" className="showTotal">Total Appointments:{totalAppointments}</p>  
+       <ul id="appointmentBreakDownId" className="hideTotal">
         <li>Successfull Appointments:{successfulAppointments}</li>
         <li>Cancelled Appointments:{cancelledAppointments}</li>   
        </ul>
+      </div> 
    )
  }
