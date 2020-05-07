8bfa3c07e8448c5a1fc3c2dd7cce033fa7235a27
Kaushal Regmi <regmikaushal321@gmail.com>
2020-05-07 12:46:40 +0545

    done

--- packages/ui-components/src/DoctorWithSpecializationImageComponent/DoctorWithSpecializationAndImageComponent.js
+++ packages/ui-components/src/DoctorWithSpecializationImageComponent/DoctorWithSpecializationAndImageComponent.js
@@ -2,25 +2,29 @@ import React, {memo} from 'react'
 import DefaultPic from './picture.png'
 const DoctorWithSpecializationAndImage = props => {
   return (
-        <div className="di-column">
-            {props.node.data.doctorAvatar ||props.node.data.fileUri ? (
-              <div className="data-image">
-                <img alt="PIC" src={props.node.data.doctorAvatar||props.node.data.fileUri} />{' '}
-              </div>
-            ) : (
-              <div className="data-image">
-                <img alt="PIC" src={DefaultPic} />
-              </div>
-            )}
-          
-         
-            <ul className="di-details">
-              <li>Dr. {props.node.data.doctorName || ''}</li>
-              <li>{props.node.data.specializationName ||props.node.data.specialization||''}</li>
-            </ul>
+    <div className="di-column">
+      {props.node.data.doctorAvatar || props.node.data.fileUri ? (
+        <div className="data-image">
+          <img
+            alt="PIC"
+            src={props.node.data.doctorAvatar || props.node.data.fileUri}
+          />{' '}
+        </div>
+      ) : (
+        <div className="data-image">
+          <img alt="PIC" src={DefaultPic} />
+        </div>
+      )}
 
-          </div>
-     
+      <ul className="di-details">
+        <li>Dr. {props.node.data.doctorName || ''}</li>
+        <li>
+          {props.node.data.specializationName ||
+            props.node.data.specialization ||
+            ''}
+        </li>
+      </ul>
+    </div>
   )
 }
 
