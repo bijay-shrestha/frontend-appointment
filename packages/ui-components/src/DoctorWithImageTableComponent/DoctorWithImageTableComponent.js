68798164d938b4bf6b0506156c4e40495a516052
Kaushal Regmi <regmikaushal321@gmail.com>
2020-05-07 12:59:35 +0545

    done

--- packages/ui-components/src/DoctorWithImageTableComponent/DoctorWithImageTableComponent.js
+++ packages/ui-components/src/DoctorWithImageTableComponent/DoctorWithImageTableComponent.js
@@ -3,30 +3,24 @@ import DefaultPic from './picture.png'
 //import {Row, Col, Container} from 'react-bootstrap'
 const DoctorWithImage = props => {
   return (
-  <div className="di-column">
-      
-         
-            {props.node.data.doctorAvatar || props.node.data.fileUri ? (
-              <div className="data-image">
-                <img
-                  alt="PIC"
-                  src={props.node.data.doctorAvatar || props.node.data.fileUri}
-                />{' '}
-              </div>
-            ) : (
-              <div className="data-image">
-                <img alt="PIC" src={DefaultPic} />
-              </div>
-            )}
-         
-         
-            <ul>
-              <li>Dr. {props.node.data.doctorName || ''}</li>
-            </ul>
-         
-          </div>
-      
-  
+    <div className="di-column">
+      {props.node.data.doctorAvatar || props.node.data.fileUri ? (
+        <div className="data-image">
+          <img
+            alt="PIC"
+            src={props.node.data.doctorAvatar || props.node.data.fileUri}
+          />{' '}
+        </div>
+      ) : (
+        <div className="data-image">
+          <img alt="PIC" src={DefaultPic} />
+        </div>
+      )}
+
+      <ul>
+        <li>Dr. {props.node.data.doctorName || ''}</li>
+      </ul>
+    </div>
   )
 }
 
