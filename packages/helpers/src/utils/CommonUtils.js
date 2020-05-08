c2b49d37e89bde27ced1ac70457694dca4305e29
Kaushal Regmi <regmikaushal321@gmail.com>
2020-05-08 17:51:46 +0545

    done

--- packages/helpers/src/utils/CommonUtils.js
+++ packages/helpers/src/utils/CommonUtils.js
@@ -25,15 +25,21 @@ export const getUserNameHospitalIdAndAdminId = token => {
 
 export const filterTableDataWithGivenStatus = (status,filterData) => filterData.filter(datum =>datum.status===status);
 
-export const matchTheWordEveryPossible = (stringToMatch,stringToMatchAgainst) =>{
-  let us = stringToMatch.toLowerCase();
-  let i = 0;
-  for (let c in stringToMatchAgainst.toLowerCase().split("")) {
-      let next = us.indexOf(c, i);
-      if (next < 0) {
-          return false;
+export const matchTheWordEveryPossible = (stringToMatchAgainst,stringToMatch) =>{
+  var us = stringToMatch.toLowerCase();
+  var i = 0;
+  {
+      var array14219 = (stringToMatchAgainst.toLowerCase()).split('');
+      for (var index14218 = 0; index14218 < array14219.length; index14218++) {
+          var c = array14219[index14218];
+          {
+              var next = us.indexOf(c, i);
+              if (next < 0) {
+                  return false;
+              }
+              i = next + 1;
+          }
       }
-      i = next+1;
   }
   return true;
 } 
