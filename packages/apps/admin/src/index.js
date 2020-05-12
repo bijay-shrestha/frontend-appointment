f6b17346dcb9b535b829b9566f379a166f98ae94
Kaushal Regmi <regmikaushal321@gmail.com>
2020-05-12 13:30:26 +0545

    done

--- packages/apps/admin/src/index.js
+++ packages/apps/admin/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 //import './index.css';
 import * as serviceWorker from './serviceWorker'
 import {Apps} from './configs'
-import {EnvironmentVariableGetter} from '@frontend-appointment/helpers'
-import {logoutUser} from '@frontend-appointment/thunk-middleware'
-window.addEventListener('storage', e => {
-  if (
-    e.key === EnvironmentVariableGetter.AUTH_TOKEN &&
-    e.oldValue &&
-    !e.newValue
-  ) {
-    localStorage.clear()
-    store.dispatch(logoutUser())
-  }
-})
+// import {EnvironmentVariableGetter} from '@frontend-appointment/helpers'
+// import {logoutUser} from '@frontend-appointment/thunk-middleware'
+// window.addEventListener('storage', e => {
+//   if (
+//     e.key === EnvironmentVariableGetter.AUTH_TOKEN &&
+//     e.oldValue &&
+//     !e.newValue
+//   ) {
+//     localStorage.clear()
+//     store.dispatch(logoutUser())
+//   }
+// })
 ReactDOM.render(Apps, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
