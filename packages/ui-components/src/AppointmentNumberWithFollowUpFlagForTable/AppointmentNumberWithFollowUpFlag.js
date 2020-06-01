b4221d80fff55e4163caba648c98a250a0f81634
sabu shakya <sabu.shakya@f1soft.com>
2020-06-01 16:59:09 +0545

    Fix appointment log follow up tag issue
    Took 1 hour 24 minutes

--- packages/ui-components/src/AppointmentNumberWithFollowUpFlagForTable/AppointmentNumberWithFollowUpFlag.js
+++ packages/ui-components/src/AppointmentNumberWithFollowUpFlagForTable/AppointmentNumberWithFollowUpFlag.js
@@ -1,11 +1,12 @@
 import React, {memo} from 'react'
+
 const AppointmentNumberWithFollowUpFlag = props => {
     return (
         <>
             <ul className="patient-column">
-                <li>{props.node.data.appointmentNumber||props.node.data.apptNumber}</li>
+                <li>{props.node.data.appointmentNumber || props.node.data.apptNumber}</li>
                 {
-                    props.node.data.isFollowUp === 'N' ?
+                    props.node.data.isFollowUp === 'Y' ?
                         <li>
                             <i className="fa fa-tag"></i>&nbsp;Followup
                             {' '}
