ddb70880876b4a515f064b36a471101b67bfa133
sudinshakya <sudinshakya1@gmail.com>
2020-06-15 15:39:06 +0545

    changes

--- packages/apps/client/src/container/ClientAppointCheckinQuickMenu/AppointmentFastCheckInConfirm.js
+++ packages/apps/client/src/container/ClientAppointCheckinQuickMenu/AppointmentFastCheckInConfirm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {CCopyToClipboard, CForm, CModal} from '@frontend-appointment/ui-elements'
+import {CCopyToClipboard, CForm, CModal,CButton} from '@frontend-appointment/ui-elements'
 import {Button, Row} from 'react-bootstrap'
 
 const AppointmentFastCheckInConfirm = ({
@@ -17,21 +17,21 @@ const AppointmentFastCheckInConfirm = ({
 
                         <Button variant="secondary"
                                 size="lg"
-
                                 name=""
                             // onClickHandler={e =>
                             //     onClick(e, props.node.data.id || props.node.data, 'C')
                             // }
                         >
-                            <i className="fa fa-print"/>
+                            <i className="fa fa-print"/>&nbsp;Print
                         </Button>
 
                         <CCopyToClipboard
+                       
                             id={"appointmentNumber"}
                             textToCopy={appointmentDetails.appointmentNumber}
                             children={
-                                <span><i className="fa fa-copy"/>
-                                </span>
+                                <button className="btn btn-primary btn-clip"><i className="fa fa-copy"/>Copy Appt. Number
+                                </button>
                             }
                         />
                     </Row>
@@ -45,10 +45,12 @@ const AppointmentFastCheckInConfirm = ({
                 show={showModal}
                 modalHeading={modalHeader}
                 size="lg"
+                modalHeading="Appointment Checked-In Successfully"
                 bodyChildren={bodyContent}
                 // footerChildren={footer}
                 onHide={setShowModal}
                 dialogClassName="cogent-modal"
+                size="lg"
             />
         </>
     )
