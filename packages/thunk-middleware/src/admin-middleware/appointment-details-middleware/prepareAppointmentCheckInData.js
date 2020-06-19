6bd0f736bf3ea77f9647a5ce79932a7ddcaa6b7e
sabu shakya <sabu.shakya@f1soft.com>
2020-06-19 09:30:41 +0545

    Fix check-in data prepare
    Took 20 minutes

--- packages/thunk-middleware/src/admin-middleware/appointment-details-middleware/prepareAppointmentCheckInData.js
+++ packages/thunk-middleware/src/admin-middleware/appointment-details-middleware/prepareAppointmentCheckInData.js
@@ -1,21 +1,22 @@
 import {StringUtils} from '@frontend-appointment/helpers'
-export const constructAppointmentCheckInData =(data,requestBody)=>{
-  // console.log("data",data);
-  requestBody.name=data.patientName;
-  requestBody.age=Number(data.patientAge.replace("years",""));
-  requestBody.wardNo="5"
-  requestBody.roomNo= "10"
-  requestBody.patientId= data.hospitalNumber
-  requestBody.sex= StringUtils.toTitleCase(data.patientGender)
-  requestBody.section= "ENT"
-  requestBody.mobileNo= data.mobileNumber
-  requestBody.appointmentNo= data.appointmentNumber
-  requestBody.phoneNo= data.mobileNumber
-  requestBody.emailAddress="abc@fakemail.com"
-  requestBody.vdc= "Kathmandu Metro"
-  requestBody.district= "Kathmandu"
-  requestBody.ageDay= 3
-  requestBody.ageMonth= 1;
-  requestBody.address="Samakhushi"
-  return requestBody;
+
+export const constructAppointmentCheckInData = (data, requestBody) => {
+    // console.log("data",data);
+    requestBody.name = data.patientName;
+    requestBody.age = Number(data.patientAge ? data.patientAge.replace("years", "") : data.age);
+    requestBody.wardNo = data.ward ? data.ward : "0"
+    requestBody.roomNo = data.roomNumber ? data.roomNumber : '10'
+    requestBody.patientId = data.hospitalNumber
+    requestBody.sex = StringUtils.toTitleCase(data.patientGender ? data.patientGender : data.gender)
+    requestBody.section = data.hospitalDepartmentName ? data.hospitalDepartmentName : "ENT"
+    requestBody.mobileNo = data.mobileNumber
+    requestBody.appointmentNo = data.appointmentNumber
+    requestBody.phoneNo = data.mobileNumber
+    requestBody.emailAddress = "abc@fakemail.com"
+    requestBody.vdc = "Kathmandu Metro"
+    requestBody.district = "Kathmandu"
+    requestBody.ageDay = data.ageDay ? data.ageDay : "0"
+    requestBody.ageMonth = data.ageMonth ? data.ageMonth : "0";
+    requestBody.address = data.address ? data.address: "Samakhushi"
+    return requestBody;
 }
