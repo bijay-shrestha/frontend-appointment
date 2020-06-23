595c155b6ec814cb5add811067f2adb46dad0d61
Kaushal Regmi <regmikaushal321@gmail.com>
2020-06-23 13:22:42 +0545

    minio enviroment added

---
+++ packages/helpers/src/utils/MinioUtils.js
@@ -0,0 +1,31 @@
+import Minio from 'minio'
+import {MINIO_ACCESS_KEY,MINIO_BUCKET,MINIO_ENDPOINT,MINIO_EXPIRY_TIME,MINIO_SECRET_KEY,MINIO_SSL_FLAG,MINIO_PORT} from './EnvironmentVariableGetter'
+const minio = new Minio.Client({
+  endPoint: MINIO_ENDPOINT,
+  port:process.env.NODE_ENV==='production'?'':MINIO_PORT,
+  useSSL: MINIO_SSL_FLAG,
+  accessKey: MINIO_ACCESS_KEY,
+  secretKey: MINIO_SECRET_KEY
+});
+
+
+export const getPresignedPutUrl = async name =>{
+    try {
+        const url = await minio.presignedPutObject(MINIO_BUCKET,name, MINIO_EXPIRY_TIME);
+        console.log("URL", url)
+        return url;
+  } catch (e) {
+    throw e
+  }
+
+}
+
+export const getPresignedGetUrl = async name =>{
+    try{
+        const presignedUrl = await minio.presignedUrl('GET', MINIO_BUCKET, name, MINIO_EXPIRY_TIME)
+        console.log("Presigned",presignedUrl)
+        return presignedUrl;
+  }catch(e){
+     throw e;
+  }  
+}
