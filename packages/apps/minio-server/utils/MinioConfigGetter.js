813f8ea8cf54b35fa8bce1f4bcc11be8688c4de0
Bijay Shrestha <bijay.shrestha@f1soft.com>
2020-06-25 12:15:51 +0545

    Semicolons in MinioConfigGetter

--- packages/apps/minio-server/utils/MinioConfigGetter.js
+++ packages/apps/minio-server/utils/MinioConfigGetter.js
@@ -1,11 +1,11 @@
-const config = require('config')
+const config = require('config');
 
 module.exports = function (name) {
-    const getConfig = config.get(name)
+    const getConfig = config.get(name);
    if(getConfig){
-     console.log(name, " : ", getConfig)
-     return getConfig
+     console.log(name, " : ", getConfig);
+     return getConfig;
    } 
    throw new Error("Environmental Variable " + name +
      " Missing. Please Provide It in default.json for development or production.json for production..");
-}
+};
