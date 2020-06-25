3c55ef9664d95405b1c01d7c1e4df1a8ad67bbb6
Bijay Shrestha <bijay.shrestha@f1soft.com>
2020-06-25 11:25:20 +0545

    Minor fixes.

--- packages/apps/minio-server/utils/MinioConfigGetter.js
+++ packages/apps/minio-server/utils/MinioConfigGetter.js
@@ -3,9 +3,7 @@ const config = require('config')
 module.exports = function(name){
     const getConfig = config.get(name)
    if(getConfig){
-       console.log("getConfig",getConfig);
      return getConfig
-
    } 
-   throw new Error("Environmental Variable"+name+" Missing. Please Provide It in default.json for development or production.json for production..");
+   throw new Error("Environmental Variable " + name + " Missing. Please Provide It in default.json for development or production.json for production..");
 }
