f1f9cc39e5e8229319edb0476c7973d3786c290e
Kaushal Regmi <regmikaushal321@gmail.com>
2020-06-29 17:37:06 +0545

    department repfund

--- packages/commons/src/DeparmtentRevenueAppointmentAmount.js
+++ packages/commons/src/DeparmtentRevenueAppointmentAmount.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export const DepartmentRevenueAppointmentAmount = props => {
-  const {cancelledRevenue, doctorRevenue, totalRevenue} = props.node.data
+  const {cancelledRevenue, departmentRevenue, totalRevenue} = props.node.data
   return (
     
       <div id="revenueAmountId" className="dr-amount">Rs. {totalRevenue}
       <ul id="breakDownAmountId" className="dr-details">
-        <li><i className="fa fa-money "></i> {doctorRevenue}</li>
+        <li><i className="fa fa-money "></i> {departmentRevenue}</li>
         <li><i className="fa fa-money red"></i> {cancelledRevenue}</li>
       </ul>
       </div>
