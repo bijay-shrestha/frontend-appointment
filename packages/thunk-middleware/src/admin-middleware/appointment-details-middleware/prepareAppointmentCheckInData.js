991e794c1d4b9a103770db25a2d123f6d18682df
sabu shakya <sabu.shakya@f1soft.com>
2020-07-10 18:42:51 +0545

    Check room number before setting data for checkin
    Took 37 minutes

--- packages/thunk-middleware/src/admin-middleware/appointment-details-middleware/prepareAppointmentCheckInData.js
+++ packages/thunk-middleware/src/admin-middleware/appointment-details-middleware/prepareAppointmentCheckInData.js
@@ -1,21 +1,22 @@
 import {StringUtils} from '@frontend-appointment/helpers'
-export const constructAppointmentCheckInData =(data,requestBody)=>{
-  // console.log("data",data);
-  requestBody.name=data.patientName;
-  requestBody.age=data.patientAge?Number(data.patientAge.replace("years","")):data.age;
-  requestBody.wardNo=data.wardNumber
-  requestBody.roomNo= data.roomNumber.includes('-')?data.roomNumber.split('-')[1]:data.roomNumber
-  requestBody.patientId= data.hospitalNumber
-  requestBody.sex= StringUtils.toTitleCase(data.gender||data.patientGender)
-  requestBody.section= data.hospitalDepartmentName
-  requestBody.mobileNo= data.mobileNumber
-  requestBody.appointmentNo= data.appointmentNumber
-  requestBody.phoneNo= data.mobileNumber
-  requestBody.email=data.emailAddress||'abc@gmail.com'
-  requestBody.vdc= data.vdcOrMunicipality
-  requestBody.district= data.district
-  requestBody.ageDay= data.ageDay
-  requestBody.ageMonth= data.ageMonth;
-  requestBody.address=data.address
-  return requestBody;
+
+export const constructAppointmentCheckInData = (data, requestBody) => {
+    // console.log("data",data);
+    requestBody.name = data.patientName;
+    requestBody.age = data.patientAge ? Number(data.patientAge.replace("years", "")) : data.age;
+    requestBody.wardNo = data.wardNumber
+    requestBody.roomNo = data.roomNumber && data.roomNumber.includes('-') ? data.roomNumber.split('-')[1] : data.roomNumber
+    requestBody.patientId = data.hospitalNumber
+    requestBody.sex = StringUtils.toTitleCase(data.gender || data.patientGender)
+    requestBody.section = data.hospitalDepartmentName
+    requestBody.mobileNo = data.mobileNumber
+    requestBody.appointmentNo = data.appointmentNumber
+    requestBody.phoneNo = data.mobileNumber
+    requestBody.email = data.emailAddress || 'abc@gmail.com'
+    requestBody.vdc = data.vdcOrMunicipality
+    requestBody.district = data.district
+    requestBody.ageDay = data.ageDay
+    requestBody.ageMonth = data.ageMonth;
+    requestBody.address = data.address
+    return requestBody;
 }
