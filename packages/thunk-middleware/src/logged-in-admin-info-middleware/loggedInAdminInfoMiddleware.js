071a317542e5a984d60ed5f00776fd6767a27b26
sabu shakya <sabu.shakya@f1soft.com>
2020-07-17 11:37:41 +0545

    Remove presigned api fetch in logged in adminInfo
    Took 4 minutes

--- packages/thunk-middleware/src/logged-in-admin-info-middleware/loggedInAdminInfoMiddleware.js
+++ packages/thunk-middleware/src/logged-in-admin-info-middleware/loggedInAdminInfoMiddleware.js
@@ -9,8 +9,8 @@ export const fetchLoggedInAdminUserInfo = (path, data) => async dispatch => {
     try {
         let adminInfoResponse = await Axios.put(path, data);
         let dataWithFileUri = adminInfoResponse.data
-        dataWithFileUri.fileLocation = dataWithFileUri.fileUri
-        dataWithFileUri.fileUri = await MinioMiddleware.fetchPresignedUrlForGetOperation(dataWithFileUri.fileUri)
+        // dataWithFileUri.fileLocation = dataWithFileUri.fileUri
+        // dataWithFileUri.fileUri = await MinioMiddleware.fetchPresignedUrlForGetOperation(dataWithFileUri.fileUri)
         await AdminInfoUtils.saveLoggedInAdminInfo(dataWithFileUri);
         dispatch(LoggedInAdminInfoActions.loggedInAdminInfoFetchSuccess(dataWithFileUri));
         return adminInfoResponse.data;
