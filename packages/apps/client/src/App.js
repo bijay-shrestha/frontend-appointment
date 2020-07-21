daa353544a9b5893410eb149f1895cbf1d9af640
sudinshakya <sudinshakya1@gmail.com>
2020-07-21 12:34:50 +0545

    material-icon

--- packages/apps/client/src/App.js
+++ packages/apps/client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ClientContainer from "./container/ClientContainer";
 import '@frontend-appointment/common-sass-client';
 import 'font-awesome/css/font-awesome.min.css';
-
+import 'material-icons/iconfont/material-icons.scss';
 // import 'react-selectize/themes/index.css'
 
 const App = props => {
